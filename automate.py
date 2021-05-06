faa05b7299397ccaa149a571363787440ce5f175
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2021-05-07 01:55:00 +0200

    elimination_epsilone

--- automate.py
+++ automate.py
@@ -1,11 +1,10 @@
 # coding: utf-8
-import math
-from Automates import *
+import re
 
-fichier = open("Automates/int2-1-FA1.txt", "r")
+fichier = open("Automates/B4-32.txt", "r")
 
 
-def lire_automate_sur_fichier(fichier):
+def lire_automate_sur_fichier(fic):
     split = fic.split()
     print(split)
 
@@ -76,15 +75,15 @@ def est_un_automate_assynchrone():
     global ASYNCHRONES
     ASYNCHRONES=[]
     while (i<NB_TRANSITIONS):
-        if (TRANSITIONS[i].find("*")):
+        if (TRANSITIONS[i].find('*') >= 0):
             asynchrone=True
             ASYNCHRONES.append(TRANSITIONS[i])
         i+=1
     if ASYNCHRONES:
-        print(ASYNCHRONES)
+        print('ASYNCHRONES :', ASYNCHRONES)
     return(asynchrone)
 
-#def elimination epsilon():
+#def elimination_epsilon():
 
 ##### main #####
 
@@ -98,10 +97,97 @@ lire_automate_sur_fichier(fic)
 print('\n')
 #afficher_automate()
 
-ecriture_automate_sur_fichier()
 print(est_un_automate_assynchrone())
 
-if not ASYNCHRONES:
-    bus = []
-    transit_epsilon = []
-    print(INITIAL_STATE)
+if ASYNCHRONES: # elimination_epsilon #TODO: 21 dans INIT à supprimer si pas de transition sortante et état final
+    print('start :', ASYNCHRONES) #TODO: FINAL_STATE
+    while ASYNCHRONES:
+
+        split_epsilon = ASYNCHRONES[0].split('*') # une transition vers un 'enfant'
+        split_epsilon.insert(1, '*')
+        #print('split_epsilon début :', ''.join(split_epsilon))
+        transit_epsilon = [] # liste des transitions vers les 'petit-enfants'
+        #print(split_epsilon[0])
+        if split_epsilon[1]=='*' and split_epsilon[0] in INITIAL_STATE and split_epsilon[2] not in INITIAL_STATE:
+            INITIAL_STATE.append(split_epsilon[2])
+            NB_INITIAL_STATE+=1
+        for i in TRANSITIONS: # recherche des 'petit-enfants'
+            if i != ASYNCHRONES[0] and split_epsilon[2] == re.split('\D+', i)[0]:
+                transit_epsilon.append(i)    
+        print('split_epsilon :', split_epsilon,' transit_epsilon :', transit_epsilon)
+        
+        for i in transit_epsilon:
+            if i.find('*') != -1 and re.split('\D+', i)[0] in INITIAL_STATE and re.split('\D+', i)[1] not in INITIAL_STATE:
+                INITIAL_STATE.append(re.split('\D+', i)[1])
+                NB_INITIAL_STATE+=1
+                if (split_epsilon[2] in INITIAL_STATE) and len(transit_epsilon)-1 == transit_epsilon.index(i):
+                    INITIAL_STATE.remove(split_epsilon[2])
+                    NB_INITIAL_STATE-=1
+            split_epsilon[1]= re.split('\d+', i)[1]  # lettre ou * : ['','a','']  # 11*14 ou 21a2 (on ignore les chiffres)
+            split_epsilon[2]= re.split('\D+', i)[1]  # chiffre ['0','0'] (on supprime la lettre)
+            if split_epsilon[1]== '*':
+                ASYNCHRONES.append(''.join(split_epsilon))
+                #print('asynchrone append ',''.join(split_epsilon))
+            TRANSITIONS.append(''.join(split_epsilon)) # création transition entre le 'parent' et les 'petit-enfants'
+            NB_TRANSITIONS+=1
+            
+            #print('transition append ',''.join(split_epsilon))
+            nb_transition_enfant = 0
+            for j in TRANSITIONS :
+                if re.split('\D+', j)[1] == re.split('\D+', i)[0]:
+                    nb_transition_enfant +=1
+            if nb_transition_enfant <= 1 :
+                if i.find('*') != -1:
+                    ASYNCHRONES.remove(i)
+                    #print('asynchrone remove ', i)
+                TRANSITIONS.remove(i)
+                NB_TRANSITIONS-=1
+                #print('transition remove ', i)
+                '''if split_epsilon[2] in FINAL_STATE:
+                    FINAL_STATE.remove(split_epsilon[2])
+                    NB_FINAL_STATE-=1'''
+                
+            #print('split_epsilon fin :', ''.join(split_epsilon))
+        #INI FIN
+        '''
+        if split_epsilon[2] in FINAL_STATE and split_epsilon[0] not in FINAL_STATE:
+                FINAL_STATE.append(split_epsilon[0])
+                NB_FINAL_STATE+=1
+        '''
+        '''for i in FINAL_STATE:
+            if i == split_epsilon[2] or i in transit_epsilon:
+                NB_FINAL_STATE+=1
+                FINAL_STATE.append(split_epsilon[0])
+                print("exit")'''
+        TRANSITIONS.remove(ASYNCHRONES[0]) # remove de l'enfant
+        NB_TRANSITIONS-=1
+        print('remove parent ', ASYNCHRONES[0], '\n')
+        del ASYNCHRONES[0]
+        #print(ASYNCHRONES)
+        
+ecriture_automate_sur_fichier()     
+            
+    # while ASYNCHRONES :
+    #   split_epsilon = ASYNCHRONES[0].strip('*') ##0
+    #   split_epsilon.insert(1, '*')
+    #   transit_epsilon = tout les split_epsilon[2] dans TRANSITIONS.strip()[0] ##1->2 et 1->6
+    #   boucle sur les 'enfants' de transit_epsilon: ##2 et 6
+    #       ajouter TRANSITIONS strip[1,2] pour split_epsilon[1,2] ##0->2 et 0->6
+    #       Si split_epsilon[1] is epsilon :
+    #           ASYNCHRONES.append(''.join(split_epsilon))
+    #       remove() enfant de TRANSITIONS ##1->2 et 1->6
+    #       remove() si enfant dans ASYNCHRONES ##1->2 et 1->6 ####### 0 transition à revoir
+    #   recherche de transit_epsilon[0] et de ses enfants dans INITIAL_STATE puis FINAL_STATE
+    #       ajouter entré et/ou sortie à INITIAL_STATE/FINAL_STATE
+    #       enlever entré et/ou sortie à INITIAL_STATE/FINAL_STATE[i]
+    #       revoir NB_INIT et NB_FINAL
+    #   TRANSITIONS[].remove() ##0->1
+    #   ASYNCHRONES[0].remove() ##0->1
+    # 
+
+
+# determinisation_et_completion_asynchrone :
+#   elimination epsilon puis deter/complet synchrone
+
+# determinisation et completion synchrone :
+
